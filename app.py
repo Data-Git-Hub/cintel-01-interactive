0c2e30be334c8ccbaf6d839970e435f9c0714dd2
Data Analytics Graduate Student <s576977@nwmissouri.edu>
2024-10-23 12:16:34 -0500

    Update app.py
    Version 3.0
    -Add variable names

--- app.py
+++ app.py
@@ -7,9 +7,20 @@ ui.page_opts(title="PyShiny App with plot", fillable=True)
 
 # (2.0) Add a slider for specifying the number of bins in the histogram.
 # (2.1) A string id ("selected_number_of_bins") that uniquely identifies this input value.
+# (2.2) A string label (e.g., "Number of Bins") to be displayed alongside the slider.
+# (2.3) An integer representing the minimum number of bins (e.g., 0).
+# (2.4) An integer representing the maximum number of bins (e.g., 1000).
+# (2.5) An integer representing the initial value of the slider at the halfway point (e.g., 500).
+# (3.0) Added variable names.
+
+min_num = 0
+max_num = 1000
+halfway_point = (min_num + max_num) / 2
 
 with ui.sidebar():
-    ui.input_slider("selected_number_of_bins", "Number of Bins", 0, 100, 20)
+    ui.input_slider(
+        "selected_number_of_bins", "Number of Bins", min_num, max_num, halfway_point
+    )
 
 
 @render.plot(alt="A histogram")
