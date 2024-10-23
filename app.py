7b9c2d7b8ce68a7c0fe074d95ea24332e4542724
Data Analytics Graduate Student <s576977@nwmissouri.edu>
2024-10-23 12:03:09 -0500

    Update app.py
    Version 2.0
    -Modified slider to change the parameters of the overall output.
    Version 2.1
    -Added string id

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from shiny.express import ui, input, render
 # (1.0) Add page options for the overall app.
 ui.page_opts(title="PyShiny App with plot", fillable=True)
 
+# (2.0) Add a slider for specifying the number of bins in the histogram.
+# (2.1) A string id ("selected_number_of_bins") that uniquely identifies this input value.
+
 with ui.sidebar():
-    ui.input_slider("n", "N", 0, 100, 20)
+    ui.input_slider("selected_number_of_bins", "Number of Bins", 0, 100, 20)
 
 
 @render.plot(alt="A histogram")
 def histogram():
     np.random.seed(19680801)
     x = 100 + 15 * np.random.randn(437)
-    plt.hist(x, input.n(), density=True)
+    plt.hist(x, input.selected_number_of_bins(), density=True)
